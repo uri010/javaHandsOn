88c1b4a6bb57d609cf202ef3fdc1038c68528050
uri010 <dbfl0461@gmail.com>
2025-01-08 12:10:17 +0900

    [Test] StringQuizTest 작성

--- src/test/java/kr/codesquad/StringQuizTest.java
+++ src/test/java/kr/codesquad/StringQuizTest.java
@@ -13,6 +13,7 @@ class StringQuizTest {
         String abc1 = "abc";
         String abc2 = new String("abc");
         // TODO 1 변수에 정의한 문자열과 new 를 통해 생성한 인스턴스가 같은 주소를 가리키는지 비교
+        assertNotSame(abc1, abc2);
     }
 
     @Test
@@ -20,6 +21,7 @@ class StringQuizTest {
     public void test_stringLongLong() throws Exception {
         String teacher = "Honux";
         // TODO 2 문자열 길이 구하기
+        assertEquals(teacher.length(), 5);
     }
 
     @Test
@@ -28,6 +30,7 @@ class StringQuizTest {
         String teacher = "Crong";
         String frontend = "Hello";
         // TODO 3 문자열 합치기
+        assertEquals(teacher + frontend, "CrongHello");
     }
 
     @Test
@@ -35,6 +38,10 @@ class StringQuizTest {
     public void testPrintStringAsAChar() throws Exception {
         String teacher = "Pobi";
         // TODO 4 string 을 char 단위로 출력
+        char[] teacherArray = teacher.toCharArray();
+        for (char c : teacherArray) {
+            System.out.println(c);
+        }
         // 출력결과:
         // P
         // o
@@ -48,6 +55,12 @@ class StringQuizTest {
         String teacher = "Dangle";
 
         // TODO 5 더하기 연산을 이용해 String을 거꾸로 출력하기
+        String reverseTeacher = "";
+        for (int i = teacher.length() - 1; i >= 0; i--) {
+            reverseTeacher += teacher.charAt(i);
+        }
+
+        System.out.println(reverseTeacher);
         // 결과: elgnaD
     }
 
@@ -59,6 +72,18 @@ class StringQuizTest {
         StringBuilder sb = new StringBuilder();
 
         // TODO 6 StringBuilder를 이용해 String을 거꾸로 출력하기
+        for (int i = developer.length() - 1; i >= 0; i--) {
+            sb.append(developer.charAt(i));
+        }
+
+        /*
+        for (char c : developer.toCharArray()) {
+            sb.insert(0, c);
+        }
+        */
+
+        System.out.println(sb);
+
         // 결과: olleZ
     }
 }
