5d0f426237819009c46f1bf11bf0b9a7f1816df8
uri010 <dbfl0461@gmail.com>
2025-01-15 11:56:59 +0900

    [Test] HashSetQuizTest 구현

--- src/test/java/kr/codesquad/HashSetQuizTest.java
+++ src/test/java/kr/codesquad/HashSetQuizTest.java
@@ -12,39 +12,39 @@ class HashSetQuizTest {
     @Test
     @DisplayName("set의 크기 확인")
     public void testSetSize() {
-        //TODO
-        assertEquals(6,);
+        assertEquals(6, hashSet.size());
 
     }
 
     @Test
     @DisplayName("set에 grape 추가 ")
     public void testPlusNewFruit() {
-        //TODO
-        assertTrue();
+        hashSet.add("grape");
+        assertTrue(hashSet.contains("grape"));
     }
 
     @Test
     @DisplayName("set에 dulian 있는지 확인")
     public void testExistDulian () {
-        //TODO
         assertTrue(hashSet.contains("dulian"));
     }
 
     @Test
     @DisplayName("set에 dulian 있으면 삭제")
     public void testDeleteDulian () {
-        //TODO
+        hashSet.remove("dulian");
         assertFalse(hashSet.contains("dulian"));
     }
 
     @Test
     @DisplayName("set의 모든 요소 출력하기")
     public void testPrintSetAllElements () {
-        // TODO
+        for (String element : hashSet) {
+            System.out.println(element);
+        }
         // 출력결과 예시:
         // apple
         // banana
-zx        // ...
+        // ...
     }
 }
